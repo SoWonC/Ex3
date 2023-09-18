efc09e1209d58691212bccd35fcb97649425e961
SoWonC <134254229+SoWonC@users.noreply.github.com>
2023-09-18 11:01:47 +0900

    /main 요청 처리

--- src/main/java/com/example/ex3/web/MyController.java
+++ src/main/java/com/example/ex3/web/MyController.java
@@ -1,15 +1,13 @@
 package com.example.ex3.web;
 
-import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@Log4j
 public class MyController {
 
     @GetMapping("/main")
     public void main(){
-        log.info("-----------------------------------------main");
+        System.out.println("===================================main");
     }
 }
